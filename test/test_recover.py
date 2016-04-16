920108c56a61c88cde02bc4cda20042544abf1a4
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2016-04-16 18:31:37 +0900

    Add test

--- test/test_recover.py
+++ test/test_recover.py
@@ -10,3 +10,5 @@ class TestRecover(unittest.TestCase):
         self.assertEqual(recover.recover("漢 漢 10 20 漢", "漢 漢1 020漢"), "漢 漢 10 20 漢")
         self.assertEqual(recover.recover("漢 漢 10 20 漢 漢", "漢 漢1 020漢 漢"), "漢 漢 10 20 漢 漢")
         self.assertEqual(recover.recover("漢 漢 , 漢 , 漢", "漢漢 , 漢 , 漢"), "漢漢 , 漢 , 漢")
+        self.assertEqual(recover.recover("我 喜 歓 surface的pro 。", "我 喜歓sur face的pro 。"), "我 喜歓 surface的pro 。")
+        self.assertEqual(recover.recover("我 喜 歓 surface的pro 。", "我 喜歓sur face的pro。"), "我 喜歓 surface的pro。")
