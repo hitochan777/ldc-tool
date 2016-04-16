ae7fc23776de8aa8934fff5a1e46629e768df418
Hitoshi Otsuki <ilikestars55@yahoo.co.jp>
2016-04-17 00:57:47 +0900

    Add convertChinesePartToZenkaku.py

---
+++ convertChinesePartToZenkaku.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import fileinput
+import re
+import argparse
+
+"""
+"""
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input file")
+    args = parser.parse_args()
+    with open(args.input,"r") as i:
+        for line in i:
+            line = line.strip()
+            if line.startswith("zh:"):
+                result = []
+                tokens = line.split(" ")
+                for token in tokens:
+                    result.append(mojimoji.han_to_zen(token))
+
+                print("zh: %s" % (" ".join(result)))
+
+            else:
+                print(line)
